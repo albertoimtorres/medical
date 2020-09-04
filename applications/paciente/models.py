1e7e2e3d168521209c6c16cf239ff1d38650764f
Alberto Moreno <albertoimtorres@gmail.com>
2020-09-04 15:52:50 -0500

    Custom table of list patients and change CharField to JSONField in model patients

--- applications/paciente/models.py
+++ applications/paciente/models.py
@@ -1,11 +1,12 @@
 from django.contrib.postgres.fields.array import ArrayField
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 # Create your models here.
 class Paciente(models.Model):
     no_expediente = models.IntegerField('Número de expediente', unique=True)
     tipo_sangre = models.CharField('Tipo de sangre', max_length=3)
-    alergias = ArrayField(models.CharField(max_length=100), size=3)
+    alergias = ArrayField(JSONField(blank=False), size=3)
 
     def __str__(self):
-        return str(self.no_expediente) + ' - ' + self.tipo_sangre + ' - ' + " - ".join(self.alergias)
+        return str(self.no_expediente) # + ' - ' + self.tipo_sangre # + ' - ' + " - ".join(self.alergias)
